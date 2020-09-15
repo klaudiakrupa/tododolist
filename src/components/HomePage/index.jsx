1b4fdb7cdbd7b6bd4bdb9bfd851eda1499344aa5
Klaudia Krupa <eklaudiakrupa@gmail.com>
2020-09-15 23:38:07 +0200

    Added kind ask for name ♥ and save it in the store.

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { SET_USER_NAME } from "./actionTypes";
 
 const HomePage = () => {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const { userName } = useSelector((state) => state.homePageState);
+  const [usernameInputValue, setUsernameInputValue] = useState("");
+
+  const onClickHandler = () =>
+    dispatch({ type: SET_USER_NAME, payload: usernameInputValue });
 
   return (
     <div>
-      <button onClick={() => history.push("plan")}>zobacz plan na dziś</button>
-      <button onClick={() => history.push("dodaj-zadanie")}>
-        dodaj nowe zadanie
-      </button>
+      {!userName && (
+        <div>
+          <div>Jak się nazywasz?</div>
+          <input onChange={(e) => setUsernameInputValue(e.target.value)} />
+          <button onClick={onClickHandler}>to jest moje imię</button>
+        </div>
+      )}
+
+      {userName && (
+        <div>
+          <div>Dzień dobry, {userName}!</div>
+          <button onClick={() => history.push("plan")}>
+            zobacz plan na dziś
+          </button>
+          <button onClick={() => history.push("dodaj-zadanie")}>
+            dodaj nowe zadanie
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/HomePage/reducer.js
+++ src/components/HomePage/reducer.js
@@ -1,6 +1,16 @@
-const initialState = {};
+import { SET_USER_NAME } from "./actionTypes";
+
+const initialState = {
+  userName: "",
+};
 
 const homePageState = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case SET_USER_NAME:
+      return { ...state, userName: action.payload };
+
+    default:
+      return state;
+  }
 };
 export default homePageState;
