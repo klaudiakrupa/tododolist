531e8255f0df843a201028c52d5451fa14390ed2
Klaudia Krupa <eklaudiakrupa@gmail.com>
2020-09-16 01:55:44 +0200

    Added logic to generate task ids

--- src/store/tasksReducer.js
+++ src/store/tasksReducer.js
@@ -1,13 +1,19 @@
 import { ADD_NEW_TASK } from "../components/NewTaskPage/actionTypes";
 
 const initialState = {
+  maxTaskId: 0,
   tasks: [],
 };
 
 const tasksState = (state = initialState, action) => {
   switch (action.type) {
     case ADD_NEW_TASK:
-      return { ...state, tasks: [...state.tasks, action.payload] };
+      const maxTaskId = ++state.maxTaskId;
+      return {
+        ...state,
+        maxTaskId: maxTaskId,
+        tasks: [...state.tasks, { ...action.payload, taskId: maxTaskId }],
+      };
 
     default:
       return state;
